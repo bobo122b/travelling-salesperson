ff5ba077b851c01c6c858cc2d5809da904c3c4fe
bobo122b <71796506+bobo122b@users.noreply.github.com>
2021-10-14 17:32:26 +0200

    adding comments

--- nextOrder.c
+++ nextOrder.c
@@ -1,6 +1,7 @@
 //This one to get the next order in the permutations
 #include "sales.h"
 
+//I actually found this algorithm on the internet, it's also implemented in "the coding train"s video on the salesman problem
 //get largest i that arr[i] < arr[i + 1]
 //get largest j that arr[largestI] < arr[j]
 //swap arr[largestI] and arr[largestJ]
@@ -11,23 +12,24 @@ int* nextOrder(int* order, int len)
     int largestI = -1, largestJ = -1;
     int i, j;
     for (i = 0; i < len - 1; i++)
-        if (order[i] < order[i + 1])
+        if (order[i] < order[i + 1])        //get the largest index that arr[i] < arr[i+1]
             largestI = i;
 
-    if (largestI == -1)
+    if (largestI == -1)                     //exit if there's no indices like that again
     {
         printf("FINISHED\n");
         return NULL;
     }
 
     for (j = 0; j < len; j++)
-        if (order[largestI] < order[j])
+        if (order[largestI] < order[j])      //get the largest index that arr[largestI] < arr[j] 
             largestJ = j;
 
-    swap(order, largestI, largestJ);
-    int* endArray = splice(order, largestI + 1, totalCities, &outlen);
-    reverse(endArray, outlen);
-    order = concat(order, totalCities-outlen, endArray, outlen);
+    swap(order, largestI, largestJ);        //swap arr[i] and arr[j]
+    //reverse arr[largestI+1....n]
+    int* endArray = splice(order, largestI + 1, totalCities, &outlen);  //cut the order array and save the cut in a variable 
+    reverse(endArray, outlen);  //reverse endArray
+    order = concat(order, totalCities-outlen, endArray, outlen);    //concatenate the reversed piece of order with the rest
     free(endArray);
     return order;
 }
