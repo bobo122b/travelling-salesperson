650a4b39d81228d8a201e6210867ba97f7e491ed
bobo122b <71796506+bobo122b@users.noreply.github.com>
2021-10-14 17:39:02 +0200

    adding comments

--- main.c
+++ main.c
@@ -2,35 +2,35 @@
 
 int main()
 {
-    srand(time(NULL));
+    srand(time(NULL));                                      //for random seed
     city cities[totalCities];
     int i;
     int totalPermutations;
-    int outlen;
     int* order = (int*)calloc(totalCities, sizeof(int));
     double recordDistance;
     int* bestEver;
+    //filling the cities array with random numbers
     for (i = 0; i < totalCities; i++)
     {
         cities[i].x = rand() % width;
         cities[i].y = rand() % height;
         order[i] = i;
     }
-    double d = calcDistance(cities, order, totalCities);
-    recordDistance = d;
-    bestEver = slice(order, totalCities);
-    totalPermutations = factorial(totalCities);
+    double d = calcDistance(cities, order, totalCities);    //calculating the total distance of the first order
+    recordDistance = d;                                     //storing this distance
+    bestEver = slice(order, totalCities);                   //storing the first order
+    totalPermutations = factorial(totalCities);             //calculating the total permutations that the computer will go through
     printf("%d\n", totalPermutations);
     printf("best so far %lf\n", recordDistance);
 
     while(order)
     {
-        order = nextOrder(order, totalCities);
-        d = calcDistance(cities, order, totalCities);
+        order = nextOrder(order, totalCities);              //finding the next order of cities
+        d = calcDistance(cities, order, totalCities);       //calculating the total distance with the new order
         if (d < recordDistance)
         {
-            recordDistance = d;
-            bestEver = slice(order, totalCities);
+            recordDistance = d;                             //resetting the record distance to a new distance if it is less than the previous
+            bestEver = slice(order, totalCities);           //resetting the best ever order
             printf("best so far %lf\n", recordDistance);
         }
     }
