14fa0f6d0cfca1b9b699832bc1f931952ce64f39
bobo122b <71796506+bobo122b@users.noreply.github.com>
2021-10-14 17:27:31 +0200

    adding comments

--- distance.c
+++ distance.c
@@ -1,3 +1,5 @@
+/*this file contains the dist function that calculates the distance between two cities and calcDistance that calculates the full distance between cities in a 
+  provided order*/
 #include "sales.h"
 
 // returns the distance between two points on a 2D plan
@@ -11,6 +13,7 @@ double dist(city* c1, city* c2)
     return distance;
 }
 
+// calculates the total distance which the travelling salesperson will walk if he took the provided cities in the provided order
 double calcDistance(city* cities, int* order, int ordlen)
 {
     double d, sum = 0;
@@ -20,8 +23,8 @@ double calcDistance(city* cities, int* order, int ordlen)
     {
         cityA = cities[order[i]];
         cityB = cities[order[i+1]];
-        d = dist(&cityA, &cityB);
-        sum += d;
+        d = dist(&cityA, &cityB); 
+        sum += d; //total distance
     }
     return sum;
 }
